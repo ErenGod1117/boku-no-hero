7509fbd1a8bc11eb925dd9be007e8941d96ec78f
ErenGod1117 <onepiecesagawano@gmail.com>
2022-03-29 03:33:21 -0500

    angular-activity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContenidoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
